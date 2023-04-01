b43320c6a940a9be86ac83f4aa7cb332bb332f23
xxsweet <xxsweet07@gmail.com>
2023-03-31 21:20:21 -0300

    coloquei um intervalo,na transaçao do mod dos  nomes

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string nome, sobrenome;
+Console.Write("Digite seu nome.......: "); 
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome..: ");
+sobrenome = Console.ReadLine()!;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+    Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
